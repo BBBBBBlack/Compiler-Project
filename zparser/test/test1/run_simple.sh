b5e705fb6934c1e50f672cb64219964672eba363
yJader_xx_arch <yJader@foxmail.com>
2024-05-23 23:09:34 +0800

    简单的计算测试 通过

--- zparser/test/test1/run_simple.sh
+++ zparser/test/test1/run_simple.sh
@@ -4,6 +4,4 @@ cd ../../build
 
 ./ZTableGenerator -i ../include -c ../test/test1/config_simple.json -f ../test/test1/out/FA_simple.md -t ../test/test1/parseTable_simple.md -p ../test/test1/parse_simple.cpp    
 
-./ZParser -i ../test/test1/token_simple.txt -t ../test/test1/parseTable_simple.md -o ../test/test1/out/CST_simple.md -p ../test/test1/out/process_simple.md 2> error.txt
-
-
+./ZParser -i ../test/test1/token_simple.txt -t ../test/test1/parseTable_simple.md -o ../test/test1/out/CST_simple.md -p ../test/test1/out/process_simple.md 2> error_simple.txt
