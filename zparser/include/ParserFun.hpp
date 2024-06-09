5e6a5a186a4d891870e4302aafc7e8ceb2c32bc2
black <1370441324@qq.com>
2024-06-09 16:06:22 +0800

    get_width

--- zparser/include/ParserFun.hpp
+++ zparser/include/ParserFun.hpp
@@ -6,6 +6,7 @@
 extern int offset;
 extern std::string t; // 暂存类型type
 extern int w;         // 暂存宽度width
+extern std::unordered_map<std::string, int> type_width_map;
 
 struct Temp
 {
@@ -22,4 +23,6 @@ void gen(std::string outFile, std::string contant);
  * @brief 获取数组元素类型
  */
 std::string get_elem(std::string s_type);
+
+int get_width(std::string s_type);
 #endif // PARSER_FUN_HPP
--- zparser/src/ParserFun.cpp
+++ zparser/src/ParserFun.cpp
@@ -1,5 +1,11 @@
 #include <ParserFun.hpp>
 
+std::unordered_map<std::string, int> type_width_map = {
+    {"int", 4},
+    {"float", 4},
+    {"double", 8},
+    {"real", 8},
+    {"char", 1}};
 int Temp::tempNum = 0;
 std::string Temp::newTemp()
 {
@@ -23,4 +29,20 @@ std::string get_elem(std::string s_type)
         s_type = type;
     }
     return s_type;
+}
+
+int get_width(std::string s_type)
+{
+    int res = 1;
+    std::regex pattern(R"(array\((\d+),\s*(\w+|array\(.*?\))\))");
+    std::smatch match;
+    while (std::regex_search(s_type, match, pattern))
+    {
+        int size = std::stoi(match[1].str());
+        std::string type = match[2].str();
+        s_type = type;
+        res *= size;
+    }
+    res *= type_width_map[s_type];
+    return res;
 }
